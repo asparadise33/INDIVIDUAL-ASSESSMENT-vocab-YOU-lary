7475bc552c20db80d8fc8b07fca49269c32e2938
asparadise33 <abby.paradise@gmail.com>
2024-06-05 13:31:32 -0500

    CARDS ON DOM! ITS WORKINGgit add -A!

--- api/vocabData.js
+++ api/vocabData.js
@@ -14,44 +14,43 @@ const getCards = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-const createCard = (payload) => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/vocabCards.json`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then((data) => resolve(data))
-    .catch(reject);
-});
+// const createCard = (payload) => new Promise((resolve, reject) => {
+//   fetch(`${endpoint}/vocabCards.json`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(payload),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => resolve(data))
+//     .catch(reject);
+// });
 
-const deleteCard = (firebaseKey) => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/vocabCards/${firebaseKey}.json`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => resolve(data))
-    .catch(reject);
-});
+// const deleteCard = (firebaseKey) => new Promise((resolve, reject) => {
+//   fetch(`${endpoint}/vocabCards/${firebaseKey}.json`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => resolve(data))
+//     .catch(reject);
+// });
 
-const updateCard = (payload) => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/vocabCards/${payload.firebaseKey}.json`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then(resolve)
-    .catch(reject);
-});
+// const updateCard = (payload) => new Promise((resolve, reject) => {
+//   fetch(`${endpoint}/vocabCards/${payload.firebaseKey}.json`, {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(payload),
+//   })
+//     .then((response) => response.json())
+//     .then(resolve)
+//     .catch(reject);
+// });
 
-export {
-  getCards, createCard, deleteCard, updateCard
-};
+export default getCards; // createCard, // deleteCard, // updateCard
+// };
