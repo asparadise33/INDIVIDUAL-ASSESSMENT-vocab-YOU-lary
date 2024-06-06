047cc68de2c7a28578e7ae4e7a44c1bc37e2bc64
asparadise33 <abby.paradise@gmail.com>
2024-06-05 20:37:18 -0500

    delete function up and running

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,34 +1,33 @@
-// // import addVocabCard from '../components/form/addVocabCardForm';
-// import getCards from '../api/vocabData';
-// import { showCards } from '../pages/vocab';
+// import addVocabCard from '../components/form/addVocabCardForm';
+import { getCards, deleteCard } from '../api/vocabData';
+import { showCards } from '../pages/vocab';
 
-// const domEvents = () => {
-//   document.querySelector('#main-container').addEventListener('click', (e) => {
-//     if (e.target.id.includes('delete-card-btn')) {
-//       // eslint-disable-next-line no-alert
-//       if (window.confirm('Want to delete?')) {
-//         console.warn('CLICKED DELETE CARD', e.target.id);
-//         const [, firebaseKey] = e.target.id.split('--');
+const domEvents = () => {
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+    if (e.target.id.includes('delete-card-btn')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Want to delete?')) {
+        console.warn('CLICKED DELETE CARD', e.target.id);
+        const [, firebaseKey] = e.target.id.split('--');
 
-//         deleteCard(firebaseKey).then(() => {
-//           getCards().then(showCards);
-//         });
-//       }
-//     }
+        deleteCard(firebaseKey).then(() => {
+          getCards().then(showCards);
+        });
+      }
+    }
 
-//     // if (e.target.id.includes('add-card-btn')) {
-//     //   addVocabCard();
-//     //   console.warn('ADD CARD');
-//     // }
+    // if (e.target.id.includes('add-card-btn')) {
+    //   addVocabCard();
+    //   console.warn('ADD CARD');
+    // }
 
-//     // TODO: CLICK EVENT EDITING/UPDATING A BOOK---DONE
-//     // if (e.target.id.includes('edit-book-btn')) {
-//     // const [, firebaseKey] = e.target.id.split('--');
+    // if (e.target.id.includes('edit-book-btn')) {
+    // const [, firebaseKey] = e.target.id.split('--');
 
-//   // getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
-//   // console.warn('EDIT BOOK', e.target.id);
-//     // }
-//   });
-// };
+    // getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
+    // console.warn('EDIT BOOK', e.target.id);
+    // }
+  });
+};
 
-// export default domEvents;
+export default domEvents;
